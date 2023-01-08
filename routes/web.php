fac2afb1be308bf0092d9c1757109a63824cbec7
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2023-01-08 10:53:26 +0530

    Modification in controllers

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
-
-Route::get('/', 'App\Http\Controllers\ImageController@create');
-Route::post('/', 'App\Http\Controllers\ImageController@store');
-Route::get('/{image}', 'App\Http\Controllers\ImageController@show');
+//
+//Route::get('/', 'App\Http\Controllers\ImageController@create');
+//Route::post('/', 'App\Http\Controllers\ImageController@store');
+//Route::get('/{image}', 'App\Http\Controllers\ImageController@show');
 
 Route::get('/', 'App\Http\Controllers\UploadController@index');
 Route::post('/store', 'App\Http\Controllers\UploadController@store');
