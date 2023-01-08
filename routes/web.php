a03766598695bcd234b288d4294c36b90c2ebc6e
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2023-01-08 11:22:06 +0530

    Change store router

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@
 //Route::post('/', 'App\Http\Controllers\ImageController@store');
 //Route::get('/{image}', 'App\Http\Controllers\ImageController@show');
 
-Route::get('/', 'App\Http\Controllers\UploadController@index');
+Route::get('/store', 'App\Http\Controllers\UploadController@index');
 Route::get('/test', 'App\Http\Controllers\UploadController@test');
 Route::post('/store', 'App\Http\Controllers\UploadController@store');
 Route::get('download/{file}','App\Http\Controllers\UploadController@download');
