1270b95372e4d92845f02575ff3cb8636fe66947
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2023-01-08 11:51:01 +0530

    test route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
-//
-//Route::get('/', 'App\Http\Controllers\ImageController@create');
-//Route::post('/', 'App\Http\Controllers\ImageController@store');
-//Route::get('/{image}', 'App\Http\Controllers\ImageController@show');
 
-Route::get('/store', 'App\Http\Controllers\UploadController@index');
-Route::get('/test', 'App\Http\Controllers\UploadController@test');
+Route::get('/', 'App\Http\Controllers\ImageController@create');
+Route::get('/test', 'App\Http\Controllers\ImageController@test');
+Route::post('/', 'App\Http\Controllers\ImageController@store');
+Route::get('/{image}', 'App\Http\Controllers\ImageController@show');
+
+//Route::get('/', 'App\Http\Controllers\UploadController@index');
 //Route::post('/store', 'App\Http\Controllers\UploadController@store');
-Route::get('download/{file}','App\Http\Controllers\UploadController@download');
-Route::delete('remove/{file}','App\Http\Controllers\UploadController@destroy');
+//Route::get('download/{file}','App\Http\Controllers\UploadController@download');
+//Route::delete('remove/{file}','App\Http\Controllers\UploadController@destroy');
