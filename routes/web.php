c85711bbe5bd0d56f2ceaf4aa29dda6554717917
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2023-01-08 11:24:37 +0530

    Remove store

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@
 
 Route::get('/store', 'App\Http\Controllers\UploadController@index');
 Route::get('/test', 'App\Http\Controllers\UploadController@test');
-Route::post('/store', 'App\Http\Controllers\UploadController@store');
+//Route::post('/store', 'App\Http\Controllers\UploadController@store');
 Route::get('download/{file}','App\Http\Controllers\UploadController@download');
 Route::delete('remove/{file}','App\Http\Controllers\UploadController@destroy');
